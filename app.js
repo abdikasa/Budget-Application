cb1630b30597bdbc0f78272a623d37f6f758d0f6
Said-Abdikarim <kareemfrmto@gmail.com>
2019-06-27 20:44:25 -0400

    We can add income ad expenses now & Modules and controllers for budget, ui and link controller.
    Created objects for income and expenses, return objects that contain methods, return DOM strings, insertAdjacentHTML, add expenses and income.

--- app.js
+++ app.js
@@ -2,6 +2,63 @@
 
 //Budget Controller
 let budgetCtrl = (function () {
+
+    let Expense = function (desc, value, id) {
+        this.desc = desc;
+        this.value = value;
+        this.id = id;
+    }
+
+    let Income = function (desc, value, id) {
+        this.desc = desc;
+        this.value = value;
+        this.id = id;
+    }
+
+    const data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        total: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    return {
+        addItem: function (type, desc, value) {
+            let newItem, id;
+            //Create ID
+            //id = data.allItems['exp' || 'inc'][index]
+            //Check if an array is empty first.
+
+            if (data.allItems[type].length > 0) {
+                id = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                id = 0;
+            }
+            //Determine which array income or  expenses
+            //Create a new object
+            if (type === 'exp') {
+                newItem = new Expense(desc, value, id);
+            } else if (type === 'inc') {
+                newItem = new Income(desc, value, id);
+            }
+
+            //Store the objects in their parent container. Then return the object.
+            //remember arrays can be accesedlike array["hello"] === array.hello
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+
+        toStringObj: function () {
+            console.log(data);
+        }
+    }
+
+
+
 })();
 
 
@@ -17,7 +74,9 @@ let uiCtrl = (function () {
         inputType: '.add__type',
         inputDesc: '.add__description',
         inputAmt: '.add__value',
-        addBtn: '.add__btn'
+        addBtn: '.add__btn',
+        incList: '.income__list',
+        expList: '.expenses__list'
 
     }
 
@@ -36,10 +95,29 @@ let uiCtrl = (function () {
         //getter
         getDOMStrings: function () {
             return DOMstrings;
+        },
+
+        addItemToUI: function (obj, type) {
+            var html, newHTML, which;
+            //HTML strings placeholder text
+
+            if (type === 'inc') {
+                which = DOMstrings.incList;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%desc%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                which = DOMstrings.expList;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%desc%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">10%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+            //Replace placeholder with data
+            newHTML = html.replace("%id%", obj.id)
+            newHTML = newHTML.replace("%desc%", obj.desc)
+            newHTML = newHTML.replace("%value%", obj.value)
+
+            //Insert HTML into the DOM 
+            document.querySelector(which).insertAdjacentHTML('beforeend', newHTML);
         }
-
     }
-
 })();
 
 
@@ -48,12 +126,29 @@ let linkCtrl = (function (budget, ui) {
     //gets the stored strings from the created object in the UI controller.
     let uiStrings = ui.getDOMStrings();
 
+    let addItem = function () {
+        let input, newItem;
+        //What needs to be done when button is clicked.
+        //1. Get the input data
+        input = ui.getUserInput();
+
+        //2. Add item to budgetCtrl
+        newItem = budget.addItem(input.type, input.desc, input.amt);
+
+        //3. Add item to UI
+        ui.addItemToUI(newItem, input.type);
+
+        //4. Calc the budget amount.
+        //5. Display budget in the UI.
+    }
+
+
     //Store DOM Listeners Here
     //We wrapped the event listeners inside a function expression
     //Added listener for enter button.
 
     let runEvents = function () {
-        document.querySelector(uiStrings.addBtn).addEventListener('click', additem)
+        document.querySelector(uiStrings.addBtn).addEventListener('click', addItem)
         document.querySelector(uiStrings.addBtn).addEventListener('keypress', function () {
             //Check for enter key pressed
             if (event.which === 13 || event.keyCode === 13) {
@@ -62,21 +157,10 @@ let linkCtrl = (function (budget, ui) {
         })
     }
 
-    let addItem = function () {
-        //What needs to be done when button is clicked.
-        //1. Get the input data
-        const input = ui.getUserInput();
-
-        //2. Add item to budgetCtrl
-
-        
-        //3. Add item to UI
-        //4. Calc the budget amount.
-        //5. Display budget in the UI.
-    }
 
     //Return DOM as a public method
     //Now we need to call the method init outside the IIFE to have the program actually do something.
+
     return {
         init: function () {
             console.log('Start App')
